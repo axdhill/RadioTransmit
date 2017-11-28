09d778ff963302e471a7515c658584094deccb8a
Alex <axdhill@gmail.com>
2017-11-28 16:29:27 -0600

    fixes

--- test.cpp
+++ test.cpp
@@ -5,11 +5,12 @@
 
 int main() {
 	RadioDevice radio("/dev/ttyUSB0");
+
 	for(int i  = 0; i < 1000; i++) {
 		usleep(100000); 
-		printf(radio.latest());
+		printf("%s\n",radio.latest());
 	}
-	return 0
+	return 0;
 
 	
 }
