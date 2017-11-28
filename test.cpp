52f850a46fccd6897b51883b6bda6455d8f02b33
Alex <axdhill@gmail.com>
2017-11-28 16:25:56 -0600

    new test code

--- test.cpp
+++ test.cpp
@@ -1,9 +1,15 @@
 #include "RadioDevice.hpp"
 
+#include <time.h>
 
 
 int main() {
 	RadioDevice radio("/dev/ttyUSB0");
+	for(int i  = 0; i < 1000; i++) {
+		usleep(100000); 
+		printf(radio.latest());
+	}
+	return 0
 
 	
 }
